17f740121fc3a19e5d321691f970315aec45bc48
LauKap <wellslau@foxmail.com>
2022-03-17 20:04:27 +0800

    Third

--- src/main/java/com/wellsConcern/Test.java
+++ src/main/java/com/wellsConcern/Test.java
@@ -7,5 +7,6 @@ public class Test
         System.out.println("Hello Git!");
         System.out.println("Second version");
         System.out.println("Check out to hot-fix");
+        System.out.println("master collision");
     }
 }
